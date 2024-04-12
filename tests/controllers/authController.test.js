d22eef7240bec0e213738dce41d817c8bb89eb64
jusvel <justinas.veligoras@gmail.com>
2024-04-12 18:38:57 +0300

    Created signup test

---
+++ tests/controllers/authController.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+
+describe("Auth Controller", () => {
+  //CHANGE EMAIL TO A UNIQUE EMAIL EVERYTIME YOU RUN THE TEST
+  const email = "test1@email.com";
+
+  describe("POST /signup", () => {
+    it("should create a new user and return a token", async () => {
+      const response = await request("127.0.0.1:3000")
+        .post("/api/v1/users/signup")
+        .send({
+          name: "John Doe",
+          email: email,
+          password: "password123",
+          passwordConfirm: "password123",
+        });
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toHaveProperty("token");
+      expect(response.body).toHaveProperty("data.user");
+      expect(response.body.data.user.email).toBe(email);
+    });
+  });
+});
