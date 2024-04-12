9fe84e18aa2f2a7ff517b242100ea2f10608eac2
jusvel <justinas.veligoras@gmail.com>
2024-04-12 18:41:15 +0300

    Created logout test

--- tests/controllers/authController.test.js
+++ tests/controllers/authController.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 
 describe("Auth Controller", () => {
   //CHANGE EMAIL TO A UNIQUE EMAIL EVERYTIME YOU RUN THE TEST
-  const email = "test1@email.com";
+  const email = "test9@email.com";
 
   describe("POST /signup", () => {
     it("should create a new user and return a token", async () => {
@@ -20,7 +20,7 @@ describe("Auth Controller", () => {
       expect(response.body.data.user.email).toBe(email);
     });
   });
-  
+
   describe("POST /login", () => {
     it("should log in a user and return a token", async () => {
       const response = await request("127.0.0.1:3000")
@@ -29,12 +29,22 @@ describe("Auth Controller", () => {
           email: email,
           password: "password123",
         });
-      console.log(response);
 
       expect(response.statusCode).toBe(200);
       expect(response.body).toHaveProperty("token");
       expect(response.body).toHaveProperty("data.user");
       expect(response.body.data.user.email).toBe(email);
+    });
+  });
+
+  describe("GET /logout", () => {
+    it("should log out the user", async () => {
+      const response = await request("127.0.0.1:3000").get(
+        "/api/v1/users/logout"
+      );
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toHaveProperty("status", "success");
     });
   });
 });
