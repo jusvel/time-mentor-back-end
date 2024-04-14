ea66c01b55d25575ba1df96a713b4ab7f80a4d4a
jusvel <justinas.veligoras@gmail.com>
2024-04-14 23:24:11 +0300

    Created a failing test for filtering by difficulty

--- tests/controllers/taskController.test.js
+++ tests/controllers/taskController.test.js
@@ -126,4 +126,43 @@ describe("GET /tasks/search", () => {
 
     expect(res.statusCode).toBe(404);
   });
+
+  it("should return tasks with the specified difficulty level", async () => {
+    const mockTasks = [
+      {
+        _id: "task1Id",
+        title: "Task 1",
+        subject: "Subject 1",
+        user: "65f4269e9e8ca78b953c9fb6",
+        difficulty: "easy",
+      },
+      {
+        _id: "task2Id",
+        title: "Task 2",
+        subject: "Subject 2",
+        user: "65f4269e9e8ca78b953c9fb6",
+        difficulty: "medium",
+      },
+      {
+        _id: "task3Id",
+        title: "Task 3",
+        subject: "Subject 3",
+        user: "65f4269e9e8ca78b953c9fb6",
+        difficulty: "hard",
+      },
+    ];
+
+    const findSpy = jest.spyOn(Task, "find");
+    findSpy.mockImplementation(async ({ difficulty }) => {
+      return mockTasks.filter((task) => task.difficulty === difficulty);
+    });
+
+    const res = await request(app)
+      .get("/api/v1/tasks/search?difficulty=easy")
+      .set("Cookie", [`jwt=${token}`]);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.data.tasks.length).toBe(1);
+    expect(res.body.data.tasks[0].difficulty).toBe("easy");
+  });
 });
